2be8b4e8f9dcd0968d011e2eb21b0ed55f02faaa
faize3 <faizee.shk@gmail.com>
2018-11-05 17:34:32 +0530

    test pretty quick

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload. asdasdjnsdffkjsf
           </p>
-
           <a
             className="App-link"
             href="https://reactjs.org"
